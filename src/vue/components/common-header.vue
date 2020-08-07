9becf7d0e2b0be12c83e9ff4784d530abc5dd790
Ediblewildfungi <jianghhui@gmail.com>
2020-08-07 23:39:27 +0800

    chore: del voice(dev) link

--- src/vue/components/common-header.vue
+++ src/vue/components/common-header.vue
@@ -9,7 +9,7 @@
                 <ul class="nav-ul">
                     <li><a class="nav-a" href="#"><span id="this-page"> - 喵茶 - We Are Family </span></a></li>
 
-                    <li><a title="开发中" class="nav-a" href="./voice"><span>萌音</span></a></li>
+                    <li><a title="开发中" class="nav-a" href="#"><span>萌音</span></a></li>
 
                     <li><a title="开发中" class="nav-a" href="#"><span>喵云储</span></a></li>
                 </ul>
