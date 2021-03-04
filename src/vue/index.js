5d3031ba2b134e45ca889624148523fde66a1732
Ediblewildfungi <jianghhui@gmail.com>
2021-03-05 02:25:01 +0800

    refactor

--- src/vue/index.js
+++ src/vue/index.js
@@ -1,34 +1,23 @@
 import Vue from 'vue'
 import App from './app.vue'
-import VueRouter from 'vue-router'
-import  Index from './components/index-temp.vue'
-import  IndexMacro from './components/index-macro.vue'
+import router from './router/index'
+
+// use axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 
 const root = document.createElement('div')
 document.body.appendChild(root)
 
-
-// const Macro = { template: '<div>macro-dmaidjiow</div>' }
-const Test = { template: '<div>test</div>' }
-
-//
-const routes = [
-  { path: '/macro', component: IndexMacro },
-  { path: '/test', component: Test }
-]
-
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  routes: routes
-})
-
 new Vue(
   {
     router,
-    render: (h) => h(App)
+    render: (h) => h(App),
   }
 ).$mount(root)
+
+
 
 
 
--- src/vue/voice.js
+++ src/vue/voice.js
@@ -1,38 +1,18 @@
 import Vue from 'vue'
 import App from './Voice.vue'
-import VueRouter from 'vue-router'
-import axios from 'axios'
-// import VueAxios from 'vue-axios'
+import router from './router/voice'
+import axios from "axios";
+
 const root = document.createElement('div')
 document.body.appendChild(root)
-
-const Channel = { template: '<div>group {{ $route.params.gid }} Channel{{ $route.params.cid }} </div>' }
-
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-
-const routes = [
-  { path: '/group/:gid/channel/:cid', component: Channel },
-  // { path: '/voice', component: voice }
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
-]
-
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  routes: routes
-})
-
 
 const app = new Vue({
   router,
   render: (h) => h(App)
   ,mounted () {
     axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response))
+    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => (this.info = response.data.bpi))
   }
 
 
