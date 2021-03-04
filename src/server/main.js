b046ee6fded419abf5583c6cb2b680df9b9cc871
Ediblewildfungi <jianghhui@gmail.com>
2021-03-05 02:22:23 +0800

    chore: add koaJson & cors support

--- src/server/main.js
+++ src/server/main.js
@@ -3,21 +3,25 @@ const Koa = require('koa')
 const Static = require('koa-static')
 const Router = require('koa-router')
 const koaFavicon = require('koa-favicon');
+const koaJson = require('koa-json')
 const responseCz = require('./middleware/responsecz')
 const config = require("../../config")
 
-
+// 跨域
+const cors = require('@koa/cors');
 
 const Voice = require('./routes/voice')
 const Macro = require('./routes/macro')
-
 const PORT = config.server.PORT
 
 const app = new Koa()
+
+// 跨域
+app.use(cors());
+
 const router = new Router()
 
 const staticPath = '../../dist'
-
 
 app.use(Static(
     path.join(__dirname, staticPath)
@@ -26,6 +30,7 @@ app.use(Static(
 
 app.use(koaFavicon('./favicon.png'))
 app.use(responseCz())
+app.use(koaJson())
 
 app.use(async (ctx, next) => {
     ctx.sendOk({
